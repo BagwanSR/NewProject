a5f509a3447a8c0821f4569e9ca2aa60d7c3ca28
BagwanSR <shaguftabagwan4@gmail.com>
2022-12-16 13:50:57 +0530

    Demo Project

---
+++ src/demo1/practicedemo.java
@@ -0,0 +1,19 @@
+package demo1;
+
+public class practicedemo {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hey i'm uploaded on git-hub and run through jenkis");
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+}
